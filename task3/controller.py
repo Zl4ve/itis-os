a016eebfbf80281e0eef0e7fb0e1dfc22a66b369
Zl4ve <rienatos.mudarisov58@gmail.com>
2023-11-10 17:04:55 +0000

    Some refactoring

--- task3/controller.py
+++ task3/controller.py
@@ -18,7 +18,7 @@ pid1 = os.fork()
 if pid1 == 0:
 	os.close(pipe1_0[0])
 	os.dup2(pipe1_0[1], 1)
-	os.execve("/bin/python3", ["/bin/python3", "producer.py"], os.environ)
+	os.execve('/bin/python3', ['/bin/python3', 'producer.py'], os.environ)
 
 pid2 = os.fork()
 
@@ -27,7 +27,7 @@ if pid2 == 0:
 	os.close(pipe2_0[0])
 	os.dup2(pipe0_2[0], 0)
 	os.dup2(pipe2_0[1], 1)
-	os.execve("/usr/bin/bc", ["/usr/bin/bc"], os.environ)
+	os.execve('/usr/bin/bc', ['/usr/bin/bc'], os.environ)
 
 os.close(pipe1_0[1])
 os.close(pipe0_2[0])
@@ -43,6 +43,6 @@ while True:
 
 	os.write(pipe0_2[1], expression.encode('utf-8'))
 	result = os.read(pipe2_0[0], 1024).decode('utf-8')
-	print(f"{expression.strip()} = {result.strip()}")
+	print(f'{expression.strip()} = {result.strip()}')
 
 	produced += 1
